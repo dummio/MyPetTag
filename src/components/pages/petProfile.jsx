0797b43bbd7d58363547a6380201f90eb0f200a3
Sameer-H-Khan <khan.sameerh2002@gmail.com>
2023-11-08 12:41:49 -0700

    added new tag scanning redirect for authed users

--- src/components/pages/petProfile.jsx
+++ src/components/pages/petProfile.jsx
@@ -15,7 +15,12 @@ import HealthInformation from "../content/petProfileInfo/healthCard/health";
 import BehaviorInformation from "../content/petProfileInfo/behaviorCard/behavior";
 import VetProvider from "../content/petProfileInfo/vetProviderCard/vetProvider";
 import LocationService from "../content/petProfileInfo/locationServices/locationService";
-import { getUserAndPetIDFromTag, writeUserAlert, getPetData } from "../../firebaseCommands";
+import {
+  getUserAndPetIDFromTag,
+  writeUserAlert,
+  getPetData,
+  isUserAuthenticated,
+} from "../../firebaseCommands";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../images/paw.png";
 
@@ -27,6 +32,7 @@ import logo from "../../images/paw.png";
 const PetProfile = () => {
   const [uID, setUID] = useState(null);
   const [pID, setPID] = useState(null);
+  const [authed, setAuthed] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -51,10 +57,86 @@ const PetProfile = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    async function fetchAuth() {
+      const isAuthed = await isUserAuthenticated();
+      setAuthed(isAuthed);
+    }
+    fetchAuth();
+  }, []);
+
   console.log("PID THAT WE GOT: ", pID);
+  console.log("UID THAT WE GOT: ", uID);
   const navigate = useNavigate();
 
-  if (pID == "not found") {
+  if (pID === "empty" && window.location.pathname.split("/")[1] === "tag") {
+    // If the user is authenticated, navigate straight to pet creation
+    if (authed) {
+      return (
+        <React.Fragment>
+          <NavBar />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100%",
+              flex: "1 1 auto",
+            }}
+          >
+            <img
+              className="logo"
+              src={logo}
+              alt="MyPetTag"
+              width={250}
+              height={250}
+            />
+            <div className="company-title">
+              My<span style={{ color: "#75af96" }}>PetTag</span>
+            </div>
+            <p
+              style={{
+                fontSize: "24px",
+                textAlign: "center",
+                maxWidth: "500px",
+              }}
+            >
+              You scanned a new tag!{" "}
+              <Link
+                to="../create"
+                // onClick={() => navigate("../create", { replace: true })}
+                style={{
+                  textDecoration: "none",
+                  borderBottom: "2px solid #0f5738",
+                  color: "black",
+                }}
+              >
+                Click here to create a pet profile associated with this tag.
+              </Link>
+            </p>
+          </div>
+        </React.Fragment>
+      );
+      // navigate("../create", { replace: true });
+    }
+    // Otherwise, navigate to the home page with the tagID as a query param
+    else {
+      // Extract the desired query parameter value
+      const queryParameterValue = window.location.pathname.split("/")[2];
+
+      // Create a URL object from the current URL
+      const url = new URL(window.location.href);
+
+      // Set the query parameter with the extracted value
+      url.search = `parameterName=${queryParameterValue}`;
+
+      // Navigate to the new URL with the added query parameter
+      navigate(`/?scannedTag=${queryParameterValue}`, { replace: true });
+    }
+  }
+
+  if (pID === "not found") {
     console.log("where am I");
     return (
       <React.Fragment>
