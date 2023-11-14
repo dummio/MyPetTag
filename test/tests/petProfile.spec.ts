df638d658e6b90191c90185fba55fdfd5fab2e9c
FouL06 <ashtonfoulger@gmail.com>
2023-11-14 11:22:45 -0700

    added basic pet profile test

--- test/tests/petProfile.spec.ts
+++ test/tests/petProfile.spec.ts
@@ -39,8 +39,86 @@ test("Lost Pet Profile", async ({ page }) => {
   await page.getByRole("button", { name: "Find Emergency Vet" }).click();
 });
 
-// Going to need to write pet profile create test once final release is created.
+test("Create Basic Pet Profile", async ({ page }) => {
+  await page.goto("https://mypettag-5970e.web.app/");
+  await page.goto("https://mypettag-5970e.web.app/login");
+  await page.locator('input[type="text"]').click();
+  await page.locator('input[type="text"]').fill("mbelafonte@gmail.com");
+  await page.locator('input[type="password"]').click();
+  await page.locator('input[type="password"]').fill("Abcd123!");
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.waitForTimeout(3000);
+  await page.locator("#navbar-container div").first().click();
+  await page.getByRole("link", { name: "Create Pet" }).click();
+  await page.getByRole("heading", { name: "Create New Pet" }).click();
+  await page.getByRole("textbox").click();
+  await page.getByRole("textbox").fill("zvunur");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await page.getByRole("heading", { name: "Add New Pet" }).click();
+  await page.getByRole("textbox").first().click();
+  await page.getByRole("textbox").first().fill("James");
+  await page.locator(".css-19bb58m").first().click();
+  await page.getByRole("option", { name: "Dog" }).click();
+  await page
+    .locator(".css-aj8ygy-control > .css-hlgwow > .css-19bb58m")
+    .first()
+    .click();
+  await page.locator("#react-select-3-input").fill("au");
+  await page
+    .getByRole("option", { name: "australian shepherd", exact: true })
+    .click();
+  await page
+    .getByPlaceholder("Provide a brief introduction about your pet")
+    .click();
+  await page
+    .getByPlaceholder("Provide a brief introduction about your pet")
+    .fill("He is a good boy, he loves to play fetch");
+  await page.locator('input[type="date"]').fill("2023-01-01");
+  await page.getByRole("spinbutton").click();
+  await page.getByRole("spinbutton").fill("39");
+  await page
+    .locator(
+      "div:nth-child(22) > .css-aj8ygy-control > .css-hlgwow > .css-19bb58m"
+    )
+    .click();
+  await page.getByRole("option", { name: "Neutered Male" }).click();
+  await page.locator("#form-contacts-container svg").click();
+  await page.getByPlaceholder("Name").click();
+  await page.getByPlaceholder("Name").fill("Dad");
+  await page.getByPlaceholder("Phone Number").click();
+  await page.getByPlaceholder("Phone Number").fill("1111111111");
+  await page.getByPlaceholder("1234 Park Ave, City, TX 12345").click();
+  await page
+    .getByPlaceholder("1234 Park Ave, City, TX 12345")
+    .fill("1234 James Way");
+  await page.getByRole("button", { name: "Create Pet" }).click();
+  await page.getByRole("link", { name: "James" }).click();
+  await page.getByText("James", { exact: true }).click();
+  await page.getByText("australian shepherd").click();
+  await page.getByText("Dad").click();
+  await page.getByText("1234 James Way").click();
+  await page.locator("#navbar-container div").first().click();
+  await page.getByRole("link", { name: "Account" }).click();
+  await page.getByRole("heading", { name: "Welcome" }).click();
+  await page.locator("#pet-container-icons svg").nth(1).click();
+  await page.getByRole("link", { name: "James" }).getByRole("link").click();
+  await page.getByText("Are you sure you want to delete James?").click();
+  await page
+    .getByText(
+      "Warning: This will unassign the tag associated with your account."
+    )
+    .click();
+  await page.getByRole("button", { name: "Yes" }).click();
+  await page.locator("#navbar-container div").first().click();
+  await page.goto("https://mypettag-5970e.web.app/logout");
+  await page.goto("https://mypettag-5970e.web.app/");
+  await page.goto("https://mypettag-5970e.web.app/login");
+  await page.close();
+});
+
 // zvunur : code for pet creation to be used.
+
+// TODO: create detailed pet profile to be done at final release
 
 test("Edit Pet Profile", async ({ page }) => {
   await page.goto("https://mypettag-5970e.web.app/");
